b28b70172d76e86a52d50f6b7e54c8ebc7117cbf
David <david_gh86@hotmail.es>
2019-12-06 13:32:23 +0100

    add activation functions

--- neural_network.py
+++ neural_network.py
@@ -1,10 +1,23 @@
 import numpy as np
 
+# the first element of the tuple is the activation function
+# the second element is the derived function
+sigmoide = (
+    lambda x: 1 / ((1 - np.e) ** (-x)),
+    lambda x: x * (1 - x)
+)
+
+relu = (
+    lambda x: np.maximum(0, x),
+    lambda x: 1
+)
+
 
 class Neuron:
-    def __init__(self, number_of_input_values):
+    def __init__(self, number_of_input_values, activation_function):
         self.b = np.random.rand(1, 1) * 2 - 1
         self.W = np.random.rand(1, number_of_input_values) * 2 - 1
+        self.activation_function = activation_function
 
 
 class NeuralLayer:
@@ -19,6 +32,3 @@ class NeuralNetwork:
     def __init__(self, architecture):
         for index in range(len(architecture) - 1):
             self.layers.append(NeuralLayer(architecture[index], architecture[index + 1]))
-
-
-
